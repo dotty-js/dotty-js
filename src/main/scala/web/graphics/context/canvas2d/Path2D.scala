fee3b5bc688b6e987ec0ab1c3e27521362921f30
saŭlo <9655605-utenma@users.noreply.gitlab.com>
2024-11-06 11:33:23 -0700

    Path2D

--- src/main/scala/web/graphics/context/canvas2d/Path2D.scala
+++ src/main/scala/web/graphics/context/canvas2d/Path2D.scala
@@ -1,6 +1,18 @@
 package org.dottyjs.web.graphics.context.canvas2d
 
-import org.scalajs.dom.{Path2D => domPath2D}
+import scala.scalajs.js
+import scala.scalajs.js.annotation._
+import org.dottyjs.web.graphics.context.canvas2d.CanvasPath
+import org.dottyjs.web.geometry.DOMMatrix
 
-// TODO
-type Path2D = domPath2D
+@js.native
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Path2D) */
+trait Path2D extends CanvasPath:
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Path2D/addPath) */
+  def addPath(path: Path2D): Unit = js.native
+
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Path2D/addPath) */
+  def addPath(
+      path: Path2D, transform: DOMMatrix
+  ): Unit = js.native
+end Path2D
