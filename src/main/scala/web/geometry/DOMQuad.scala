b32d82de72244eaf6d9b559440de86036227c541
saŭlo <9655605-utenma@users.noreply.gitlab.com>
2024-11-06 14:04:07 -0700

    DOMQuad

---
+++ src/main/scala/web/geometry/DOMQuad.scala
@@ -0,0 +1,8 @@
+package org.dottyjs.web.geometry
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation._
+
+// TODO
+/** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/DOMQuad) */
+class DOMQuad extends js.Object
