08c85539ff11231d1765039c4f98261a1ebc43a8
saŭlo <9655605-utenma@users.noreply.gitlab.com>
2024-11-06 11:32:20 -0700

    namespaced DOMMatrix

---
+++ src/main/scala/web/geometry/DOMMatrix.scala
@@ -0,0 +1,8 @@
+package org.dottyjs.web.geometry
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation._
+
+// TODO
+/** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/DOMMatrix) */
+class DOMMatrix extends js.Object
