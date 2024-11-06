4a5af4ffcb3b0b26dcf7058d0e6a93b3c558d750
saŭlo <9655605-utenma@users.noreply.gitlab.com>
2024-11-06 11:13:57 -0700

    ctx2d import cleanup

--- src/main/scala/web/graphics/context/canvas2d/CanvasRenderingContext2D.scala
+++ src/main/scala/web/graphics/context/canvas2d/CanvasRenderingContext2D.scala
@@ -2,13 +2,6 @@ package org.dottyjs.web.graphics.context.canvas2d
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation._
-import org.scalajs.dom.{HTMLElement, CanvasFillRule}
-import org.scalajs.dom.CanvasGradient
-import org.scalajs.dom.CanvasPattern
-import org.scalajs.dom.Path2D
-import org.scalajs.dom.HTMLImageElement
-import org.scalajs.dom.HTMLVideoElement
-import org.scalajs.dom.ImageBitmap
 
 import org.dottyjs.dom.HTMLCanvasElement
 import org.dottyjs.web.graphics.context.canvas2d.CanvasCompositing
