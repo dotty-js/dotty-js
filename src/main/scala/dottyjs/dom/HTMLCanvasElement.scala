02b9b68e3fc6406d538445094bf25436b7a74d1c
saŭlo <9655605-utenma@users.noreply.gitlab.com>
2024-11-06 22:47:17 -0700

    CanvasContextMethods

--- src/main/scala/dottyjs/dom/HTMLCanvasElement.scala
+++ src/main/scala/dottyjs/dom/HTMLCanvasElement.scala
@@ -9,59 +9,22 @@ import org.scalajs.dom.MediaStream
 
 import dottyjs.web.graphics.canvas.OffscreenCanvas
 import dottyjs.web.graphics.canvas.OffscreenCanvasProperties
-import dottyjs.web.graphics.context.canvas2d.CanvasRenderingContext2D
-import dottyjs.web.graphics.context.canvas2d.Canvas2dContext2DAttributes
-import dottyjs.web.graphics.context.bitmap.ImageBitmapRenderingContext
-import dottyjs.web.graphics.context.bitmap.ImageBitmapContextAttributes
-import dottyjs.web.graphics.context.webgl.WebGLRenderingContext
-import dottyjs.web.graphics.context.webgl.WebGLContextAttributes
-import dottyjs.web.graphics.context.webgl2.WebGL2RenderingContext
+import dottyjs.web.graphics.canvas.CanvasContextMethods
 
 /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement#instance_properties) */
 @js.native
-private abstract trait HTMLCanvasElementProperties extends OffscreenCanvasProperties
+private abstract trait HTMLCanvasElementProperties
+    extends OffscreenCanvasProperties
 
 /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement#instance_methods) */
 @js.native
-private abstract trait HTMLCanvasElementMethods extends js.Object:
+private abstract trait HTMLCanvasElementMethods
+    extends js.Object with CanvasContextMethods:
   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLCanvasElement/captureStream) */
   def captureStream(): MediaStream = js.native
 
   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLCanvasElement/captureStream) */
   def captureStream(frameRequestRate: Double): MediaStream = js.native
-
-  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext) */
-  def getContext(contextId: "2d"): CanvasRenderingContext2D | Null = js.native
-
-  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext) */
-  def getContext(
-      contextId: "2d", options: Canvas2dContext2DAttributes
-  ): CanvasRenderingContext2D | Null = js.native
-
-  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext) */
-  def getContext(contextId: "webgl"): WebGLRenderingContext | Null = js.native
-
-  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext) */
-  def getContext(
-      contextId: "webgl", options: WebGLContextAttributes
-  ): WebGLRenderingContext | Null = js.native
-
-  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext) */
-  def getContext(contextId: "webgl2"): WebGL2RenderingContext | Null
-
-  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext) */
-  def getContext(
-      contextId: "webgl2", options: WebGLContextAttributes
-  ): WebGL2RenderingContext | Null
-
-  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext) */
-  def getContext(contextId: "bitmaprenderer"): ImageBitmapRenderingContext |
-    Null
-
-  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext) */
-  def getContext(
-      contextId: "bitmaprenderer", options: ImageBitmapContextAttributes
-  ): ImageBitmapRenderingContext | Null
 
   /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL) */
   def toDataURL(): String = js.native
---
+++ src/main/scala/dottyjs/web/graphics/canvas/CanvasContextMethods.scala
@@ -0,0 +1,48 @@
+package dottyjs.web.graphics.canvas
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation._
+import dottyjs.web.graphics.context.canvas2d.CanvasRenderingContext2D
+import dottyjs.web.graphics.context.canvas2d.Canvas2dContext2DAttributes
+import dottyjs.web.graphics.context.webgl.WebGLRenderingContext
+import dottyjs.web.graphics.context.webgl.WebGLContextAttributes
+import dottyjs.web.graphics.context.webgl2.WebGL2RenderingContext
+import dottyjs.web.graphics.context.bitmap.ImageBitmapRenderingContext
+import dottyjs.web.graphics.context.bitmap.ImageBitmapContextAttributes
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas/getContext) */
+@js.native
+abstract trait CanvasContextMethods extends js.Object:
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas/getContext) */
+  def getContext(contextId: "2d"): CanvasRenderingContext2D | Null = js.native
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas/getContext) */
+  def getContext(
+      contextId: "2d", options: Canvas2dContext2DAttributes
+  ): CanvasRenderingContext2D | Null = js.native
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas/getContext) */
+  def getContext(contextId: "webgl"): WebGLRenderingContext | Null = js.native
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas/getContext) */
+  def getContext(
+      contextId: "webgl", options: WebGLContextAttributes
+  ): WebGLRenderingContext | Null = js.native
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas/getContext) */
+  def getContext(contextId: "webgl2"): WebGL2RenderingContext | Null
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas/getContext) */
+  def getContext(
+      contextId: "webgl2", options: WebGLContextAttributes
+  ): WebGL2RenderingContext | Null
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas/getContext) */
+  def getContext(contextId: "bitmaprenderer"): ImageBitmapRenderingContext |
+    Null
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas/getContext) */
+  def getContext(
+      contextId: "bitmaprenderer", options: ImageBitmapContextAttributes
+  ): ImageBitmapRenderingContext | Null
+end CanvasContextMethods
