004161c8e339238a46c0c7e1aa39feb8437e1a57
saŭlo <9655605-utenma@users.noreply.gitlab.com>
2024-11-06 22:21:02 -0700

    ImageData

--- src/main/scala/dottyjs/web/graphics/canvas/ImageData.scala
+++ src/main/scala/dottyjs/web/graphics/canvas/ImageData.scala
@@ -1,22 +1,47 @@
 package dottyjs.web.graphics.canvas
 
 import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+import scala.scalajs.js.typedarray.Uint8ClampedArray
 
-// TODO make class
 @js.native
+@JSGlobal
 /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/ImageData) */
-trait ImageData extends js.Object:
+class ImageData extends js.Object:
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/ImageData/ImageData) */
+  def this(width: Int, height: Int) = this()
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/ImageData/ImageData) */
+  def this(width: Int, height: Int, settings: ImageDataSettings) = this()
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/ImageData/ImageData) */
+  def this(data: Uint8ClampedArray, width: Int) = this()
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/ImageData/ImageData) */
+  def this(data: Uint8ClampedArray, width: Int, height: Int) = this()
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/ImageData/ImageData) */
+  def this(
+      data: Uint8ClampedArray, width: Int, height: Int,
+      settings: ImageDataSettings
+  ) = this()
+
   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ImageData/colorSpace) */
-  val colorSpace: CanvasColorSpace
+  val colorSpace: CanvasColorSpace = js.native
 
   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ImageData/data) */
-  val data: js.typedarray.Uint8ClampedArray
+  val data: Uint8ClampedArray = js.native
 
-  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ImageData/height) */
-  val height: Int
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ImageData/height)
+    * unsigned
+    */
+  val height: Int = js.native
 
-  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ImageData/width) */
-  val width: Int
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ImageData/width)
+    * unsigned
+    */
+  val width: Int = js.native
 end ImageData
 
 @js.native
