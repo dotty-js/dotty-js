3e19b2ace64733706eb3c2372f13d16485e2c24e
saŭlo <9655605-utenma@users.noreply.gitlab.com>
2024-11-06 22:47:38 -0700

    OffscreenCanvas

--- src/main/scala/dottyjs/web/graphics/canvas/OffscreenCanvas.scala
+++ src/main/scala/dottyjs/web/graphics/canvas/OffscreenCanvas.scala
@@ -1,7 +1,14 @@
 package dottyjs.web.graphics.canvas
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation._
+import scala.scalajs.js.annotation.JSGlobal
+
+import org.scalajs.dom.Blob
+
+trait ImageEncodeOptions extends js.Object:
+  var quality: js.UndefOr[Double] = js.undefined
+  var `type`: js.UndefOr[String] = js.undefined
+end ImageEncodeOptions
 
 /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas#instance_properties) */
 @js.native
@@ -17,6 +24,24 @@ abstract trait OffscreenCanvasProperties extends js.Object:
   var width: Int = js.native
 end OffscreenCanvasProperties
 
-// TODO
+/** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas#instance_methods) */
+@js.native
+private abstract trait OffscreenCanvasMethods
+    extends js.Object with CanvasContextMethods:
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/OffscreenCanvas/convertToBlob) */
+  def convertToBlob(options: ImageEncodeOptions): js.Promise[Blob]
+
+  /* [MDN Reference](https://developer.mozilla.org/docs/Web/API/OffscreenCanvas/transferToImageBitmap) */
+  def transferToImageBitmap(): ImageBitmap
+end OffscreenCanvasMethods
+
+/** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas#events) */
+private abstract trait OffscreenCanvasEvents:
+  // TODO
+end OffscreenCanvasEvents
+
 /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas) */
-type OffscreenCanvas = js.Object
+@js.native
+@JSGlobal
+/** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/ImageData) */
+class OffscreenCanvas extends js.Object with OffscreenCanvasProperties
