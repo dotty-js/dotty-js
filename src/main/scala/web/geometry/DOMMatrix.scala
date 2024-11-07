a69077527f379a3117a729568e52adb8d960c84b
saŭlo <9655605-utenma@users.noreply.gitlab.com>
2024-11-06 19:33:10 -0700

    DOMMatrix2DInit

--- src/main/scala/web/geometry/DOMMatrix.scala
+++ src/main/scala/web/geometry/DOMMatrix.scala
@@ -3,6 +3,20 @@ package org.dottyjs.web.geometry
 import scala.scalajs.js
 import scala.scalajs.js.annotation._
 
-// TODO
+// Converted from tslib
+trait DOMMatrix2DInit extends js.Object:
+  var a: js.UndefOr[Double] = js.undefined
+  var b: js.UndefOr[Double] = js.undefined
+  var c: js.UndefOr[Double] = js.undefined
+  var d: js.UndefOr[Double] = js.undefined
+  var e: js.UndefOr[Double] = js.undefined
+  var f: js.UndefOr[Double] = js.undefined
+  var m11: js.UndefOr[Double] = js.undefined
+  var m12: js.UndefOr[Double] = js.undefined
+  var m21: js.UndefOr[Double] = js.undefined
+  var m22: js.UndefOr[Double] = js.undefined
+  var m41: js.UndefOr[Double] = js.undefined
+  var m42: js.UndefOr[Double] = js.undefined
+end DOMMatrix2DInit
 /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/DOMMatrix) */
 class DOMMatrix extends js.Object
