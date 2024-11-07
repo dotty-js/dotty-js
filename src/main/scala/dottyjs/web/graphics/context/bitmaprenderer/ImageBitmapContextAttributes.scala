21aa3fc978c2a027b198c52f08d475e6524d46a0
saŭlo <9655605-utenma@users.noreply.gitlab.com>
2024-11-06 22:05:22 -0700

    import cleanip

--- src/main/scala/dottyjs/web/graphics/context/bitmaprenderer/ImageBitmapContextAttributes.scala
+++ src/main/scala/dottyjs/web/graphics/context/bitmaprenderer/ImageBitmapContextAttributes.scala
@@ -1,7 +1,6 @@
 package dottyjs.web.graphics.context.bitmap
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation._
 
 /** From ts lib dom */
 trait ImageBitmapContextAttributes extends js.Object:
