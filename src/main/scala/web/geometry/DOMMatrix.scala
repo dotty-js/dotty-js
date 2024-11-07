1e24add49ac4fce1dc7b39143084f2b7bf8dbd44
saŭlo <9655605-utenma@users.noreply.gitlab.com>
2024-11-06 20:06:29 -0700

    DOMMatrixReadOnly, DOMMatrix

--- src/main/scala/web/geometry/DOMMatrix.scala
+++ src/main/scala/web/geometry/DOMMatrix.scala
@@ -91,26 +91,21 @@ abstract trait DOMMatrixReadOnlyMethods extends js.Object:
 
   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMMatrixReadOnly/scale) */
   def scale(scaleX: Double): DOMMatrix = js.native
-
   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMMatrixReadOnly/scale) */
   def scale(scaleX: Double, scaleY: Double): DOMMatrix = js.native
-
   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMMatrixReadOnly/scale) */
   def scale(
       scaleX: Double, scaleY: Double, scaleZ: Double
   ): DOMMatrix = js.native
-
   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMMatrixReadOnly/scale) */
   def scale(
       scaleX: Double, scaleY: Double, scaleZ: Double, originX: Double
   ): DOMMatrix = js.native
-
   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMMatrixReadOnly/scale) */
   def scale(
       scaleX: Double, scaleY: Double, scaleZ: Double, originX: Double,
       originY: Double
   ): DOMMatrix = js.native
-
   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMMatrixReadOnly/scale) */
   def scale(
       scaleX: Double, scaleY: Double, scaleZ: Double, originX: Double,
@@ -142,6 +137,148 @@ abstract trait DOMMatrixReadOnlyMethods extends js.Object:
   def translate(tx: Double, ty: Double, tz: Double): DOMMatrix = js.native
 end DOMMatrixReadOnlyMethods
 
+@js.native
+@JSGlobal
+object DOMMatrixReadOnly extends js.Object:
+  def fromFloat32Array(array32: Float32Array): DOMMatrixReadOnly = js.native
+  def fromFloat64Array(array64: Float64Array): DOMMatrixReadOnly = js.native
+  def fromMatrix(other: DOMMatrixInit): DOMMatrixReadOnly = js.native
+end DOMMatrixReadOnly
+
+@js.native
+@JSGlobal
+class DOMMatrixReadOnly extends DOMMatrixFields with DOMMatrixReadOnlyMethods:
+  // TODO css transform string or 6 | 16 tuple
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/DOMMatrixReadOnly/DOMMatrixReadOnly) */
+  def this(init: js.Array[Double] | String) = this()
+
+  val is2D: Boolean = js.native
+
+  val isIdentity: Boolean = js.native
+
+  val a: Double = js.native
+  val b: Double = js.native
+  val c: Double = js.native
+  val d: Double = js.native
+  val e: Double = js.native
+  val f: Double = js.native
+
+  val m11: Double = js.native
+  val m12: Double = js.native
+  val m13: Double = js.native
+  val m14: Double = js.native
+  val m21: Double = js.native
+  val m22: Double = js.native
+  val m23: Double = js.native
+  val m24: Double = js.native
+  val m31: Double = js.native
+  val m32: Double = js.native
+  val m33: Double = js.native
+  val m34: Double = js.native
+  val m41: Double = js.native
+  val m42: Double = js.native
+  val m43: Double = js.native
+  val m44: Double = js.native
+end DOMMatrixReadOnly
 
 /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/DOMMatrix) */
-class DOMMatrix extends js.Object
+@js.native
+@JSGlobal("DOMMatrix")
+class DOMMatrix extends DOMMatrixFields with DOMMatrixReadOnlyMethods:
+  // TODO css transform string or 6 | 16 tuple
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/DOMMatrix/DOMMatrix) */
+  def this(init: js.Array[Double] | String) = this()
+
+  var is2D: Boolean = js.native
+
+  var isIdentity: Boolean = js.native
+
+  var a: Double = js.native
+  var b: Double = js.native
+  var c: Double = js.native
+  var d: Double = js.native
+  var e: Double = js.native
+  var f: Double = js.native
+
+  var m11: Double = js.native
+  var m12: Double = js.native
+  var m13: Double = js.native
+  var m14: Double = js.native
+  var m21: Double = js.native
+  var m22: Double = js.native
+  var m23: Double = js.native
+  var m24: Double = js.native
+  var m31: Double = js.native
+  var m32: Double = js.native
+  var m33: Double = js.native
+  var m34: Double = js.native
+  var m41: Double = js.native
+  var m42: Double = js.native
+  var m43: Double = js.native
+  var m44: Double = js.native
+
+  def invertSelf(): DOMMatrix = js.native
+  def multiplySelf(other: DOMMatrixInit): DOMMatrix = js.native
+  def preMultiplySelf(other: DOMMatrixInit): DOMMatrix = js.native
+
+  def rotateAxisAngleSelf(x: Double): DOMMatrix = js.native
+  def rotateAxisAngleSelf(x: Double, y: Double): DOMMatrix = js.native
+  def rotateAxisAngleSelf(
+      x: Double, y: Double, z: Double
+  ): DOMMatrix = js.native
+  def rotateAxisAngleSelf(
+      x: Double, y: Double, z: Double, angle: Double
+  ): DOMMatrix = js.native
+
+  def rotateFromVectorSelf(x: Double): DOMMatrix = js.native
+  def rotateFromVectorSelf(x: Double, y: Double): DOMMatrix = js.native
+
+  def rotateSelf(rotX: Double): DOMMatrix = js.native
+  def rotateSelf(rotX: Double, rotY: Double): DOMMatrix = js.native
+  def rotateSelf(
+      rotX: Double, rotY: Double, rotZ: Double
+  ): DOMMatrix = js.native
+
+  def scale3dSelf(scale: Double): DOMMatrix = js.native
+  def scale3dSelf(scale: Double, originX: Double): DOMMatrix = js.native
+  def scale3dSelf(
+      scale: Double, originX: Double, originY: Double
+  ): DOMMatrix = js.native
+  def scale3dSelf(
+      scale: Double, originX: Double, originY: Double, originZ: Double
+  ): DOMMatrix = js.native
+
+  def scaleSelf(scaleX: Double): DOMMatrix = js.native
+  def scaleSelf(scaleX: Double, scaleY: Double): DOMMatrix = js.native
+  def scaleSelf(
+      scaleX: Double, scaleY: Double, scaleZ: Double
+  ): DOMMatrix = js.native
+  def scaleSelf(
+      scaleX: Double, scaleY: Double, scaleZ: Double, originX: Double
+  ): DOMMatrix = js.native
+  def scaleSelf(
+      scaleX: Double, scaleY: Double, scaleZ: Double, originX: Double,
+      originY: Double
+  ): DOMMatrix = js.native
+  def scaleSelf(
+      scaleX: Double, scaleY: Double, scaleZ: Double, originX: Double,
+      originY: Double, originZ: Double
+  ): DOMMatrix = js.native
+
+  def setMatrixValue(transformList: String): DOMMatrix = js.native
+
+  def skewXSelf(sx: Double): DOMMatrix = js.native
+  def skewYSelf(sy: Double): DOMMatrix = js.native
+
+  def translateSelf(tx: Double): DOMMatrix = js.native
+  def translateSelf(tx: Double, ty: Double): DOMMatrix = js.native
+  def translateSelf(tx: Double, ty: Double, tz: Double): DOMMatrix = js.native
+end DOMMatrix
+
+@js.native
+@JSGlobal
+object DOMMatrix extends js.Object:
+  def fromFloat32Array(array32: Float32Array): DOMMatrix = js.native
+  def fromFloat64Array(array64: Float64Array): DOMMatrixReadOnly = js.native
+  def fromMatrix(other: DOMMatrixInit): DOMMatrix = js.native
+end DOMMatrix
